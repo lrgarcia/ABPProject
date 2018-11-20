f765e08742544f4d03bb592126c58e6f9baadce5
lrgarcia <lrgarcia@esei.uvigo.es>
2018-11-20 13:57:55 +0100

    Campeonato ADD SHOWALL view hecho

--- View/Championship_SHOWALL_View.php
+++ View/Championship_SHOWALL_View.php
@@ -14,13 +14,42 @@ class Championship_SHOWALL_View{
 	<div class="jumbotron">
 		<div class="container">
 			<h1>VER TODAS LAS CAMPEONATOS</h1>
-			<a class="btn btn-outline-success" id="search-input" href="../Controller/Championship_Controller.php?action=ADD" role="button"Crear campeonato></a>);
 		</div>
 	</div>
 
 
 
+	<div class="container">
+
 	
+		<div class="row">
+			<div class="col-md-12">
+				<a class="btn btn-outline-success" id="search-input" href="../Controller/Championship_Controller.php?action=ADD" role="button">Crear campeonato</a>
+			</div>
+		</div>
+<?php
+		
+		foreach ($this->championships as $championship) {	
+?>
+		<div class="row section">
+			<div class="col-md-10">
+				<h2><?php echo $championship['name'] ?></h2>
+			</div>
+			<div class="col-md-2">
+				<a class="btn btn-outline-success" href="../Controller/Championship_Controller.php?action=SHOWCURRENT&id=<?php echo $championship['id'] ?>"><?php echo $strings['Ver']?></a>
+<?php
+			
+				echo('<a class="btn btn-outline-success" href="../Controller/Championship_Controller.php?action=EDIT&id=');
+				echo($championship["id"]);
+				echo('">' . $strings['Editar'] . '</a>');
+			
+?>			
+			</div>
+		</div>
+<?php
+		}
+?>
+
 
 
 	<?php
