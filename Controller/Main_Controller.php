b71e11f9bcf0f78eea2c44eea5492a2e4c359fb2
Salinillas <45066030+Salinillas@users.noreply.github.com>
2018-11-21 19:40:50 +0100

    Add files via upload

--- Controller/Main_Controller.php
+++ Controller/Main_Controller.php
@@ -2,16 +2,16 @@
 
 session_start(); 
 include '../Functions/Authentication.php';
-
+require_once '../Model/Game_Model.php';
+require_once '../Model/Championship_Model.php';
+require_once '../View/Championship_SHOWALL_View.php';
 if (!IsAuthenticated()){
 	header('Location:../index.php');
 }
 include '../Model/User_Model.php';
-include '../Model/Championship.php';
-include '../Model/Championship_Model.php';
-include '../View/Court_SHOWALL_View.php';	
-include '../View/Championship_SHOWALL_View.php';	
-include '../View/PromotedMatch_SHOWALL_View.php';	
+include '../Model/Promotion.php';
+include '../Model/Promotion_Model.php';	
+include '../View/Promotion_SHOWALL_View.php';	
 include '../View/Main_View.php';
 include '../View/MESSAGE_View.php';
 
@@ -33,9 +33,25 @@ Switch ($_REQUEST['action']){
 		$championships= $championship_model->GETALL();
 		new Championship_SHOWALL_View($championships);
 		break;
-	case 'PROMOTEDMATCH':
-		
-		new PromotedMatch_SHOWALL_View();
+	case 'PROMOTION':
+	    $promotion_model= new Promotion_Model();
+	    $game_Model=new Game_Model();
+	    $promotions= $promotion_model->GETALL();
+	    $games=array();
+	    foreach($promotions as $promotion)
+	    {
+	        $idPromotionGame=$promotion->getTdGame();
+	        $game=$game_Model->GETBYID($idPromotionGame);
+	        $date=$game->getDate();
+	        $arrayDate= explode("/", $date);
+	        $date=$arrayDate[2] . $arrayDate[1] . $arrayDate[0];
+	        $actualDate=getdate();
+	        $actualDate=$actualDate['year'] . $actualDate['mon'] . $actualDate['wday'];
+	        if($actualDate<=$date){
+	        array_push($games, $game);
+	        }
+	    }
+	    new Promotion_SHOWALL_View($games);
 		break;		
 
 	default: 
