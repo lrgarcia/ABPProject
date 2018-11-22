18ceacaee1a1f1737808d293afacf6f81992a9e9
jpvalcarcel <josuepatovalcarcel@gmail.com>
2018-11-22 17:47:23 +0100

    Add files via upload

--- View/Championship_ADD_View.php
+++ View/Championship_ADD_View.php
@@ -1,4 +1,7 @@
 <?php
+require_once '../Model/Category.php';
+require_once '../Model/Category_Model.php';
+
 class Championship_ADD_View{
 	function __construct(){	
 		$this->render();
@@ -44,51 +47,29 @@ class Championship_ADD_View{
 				<input required type="date" class="form-control" id="dateInscriptions" name="dateInscriptions" value="">
 				</div>
 			</div>
-		</div>	
+		
 
-
-
-
-
-
-
-
-
-
-
-
-		<div class = "row">
-
-			<div class="col-sm-3 nopadding">
+			<div class="col-sm-6 nopadding">
 				<div class="form-group">
 					<label>Categoria:</label>
-					<select required class="form-control" id="categoria[]" name="categoria[]">
-					<option>Masculino</option>
-    				<option>Femenino</option>
-    				<option>Mixto</option>
-    			</select>
-				</div>
-			</div>
-
-			<div class="col-sm-3 nopadding">
-				<div class="form-group">
-					<label>Modalidad:</label>
-					<select required class="form-control" id="modalidad[]" name="modalidad[]">
-					<option>1</option>
-    				<option>2</option>
-    				<option>3</option>
+					<select multiple required class="form-control" id="category[]" name="category[]" size=5>
+					<?php 
+					$categoryModel = new Category_Model();
+					$categoryArray = $categoryModel->GETALL();
+					
+					for($i=0; $i <count($categoryArray); $i++)
+					{
+					    echo "<option value='".$categoryArray[$i]->getIdCategory()."'>".$categoryArray[$i]->getCategory()." - Categoria ".$categoryArray[$i]->getModality()."</option>"; 
+					}
+					
+					
+					?>
     			</select>
 				</div>
 			</div>
 
 			<input type="text" name="login_creator" hidden value="<?php echo $_SESSION['login'] ?>">
-			<div class="col-sm-3 nopadding" id="fix-btn">
-				<div class="form-group">
-				<button id="search-input btn-left" class="btn btn-success" type="button"  onclick="add_fields();"> 
-				Añadir categoria nueva
-				</button>
-				</div>
-			</div>
+			
 
 
 		</div>
