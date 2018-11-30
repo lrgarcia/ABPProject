a4df4993e275cb063dedeca9bf1b1757652cb0e7
lrgarcia <lrgarcia@esei.uvigo.es>
2018-11-30 10:08:11 +0100

    Commit general para la ultima entrega

--- Controller/Championship_Controller.php
+++ Controller/Championship_Controller.php
@@ -98,6 +98,7 @@ Switch ($_REQUEST['action']){
         $categoryGroupModel=new CategoryGroup_Model();
         $groupModel= new Group_Model();
         $matchModel= new Match_Model();
+        $respuesta="";
        
         $alphabet =array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
         $arrayCategoryGroups = $categoryGroupModel->GETCHAMPIONSHIPGROUPS($_REQUEST['idChampionship']);
